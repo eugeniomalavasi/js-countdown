4eadb160a288e17b0188318a533508285de3d6cc
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-04-10 16:58:55 +0200

    feat: added the 0 before 10 second and minute

--- chrono/js/script.js
+++ chrono/js/script.js
@@ -1,24 +1,52 @@
+let secondsPrint = document.getElementById("seconds");
+let minutesPrint = document.getElementById("minutes")
+
+// global var to save data
+let chrono = 0; 
+let seconds = 55;
+let minutes = 0;
+
 // start button
 const btnStart = document.getElementById("start_time").addEventListener("click", function (){
     chrono = setInterval(function () {
         if (seconds < 59) {
             seconds++;
+            console.log(seconds);
         } else {
             seconds = 0;
             minutes++;
             console.log(minutes);
         }
-        console.log(seconds);
+        // output
+        // print seconds
+        if (seconds <= 9) {
+            secondsPrint.innerText = `0${seconds}`;
+        } else {
+            secondsPrint.innerText = seconds;
+        }
+        
+        // print minutes
+        if (minutes <= 9) {
+            minutesPrint.innerText = `0${minutes}`;
+        } else {
+            minutesPrint.innerText = minutes;
+        }
     }, 1000);
 });
+
 // stop button
 const btnStop = document.getElementById("stop_time").addEventListener("click", function () {
-    clearInterval(chrono)
-})
-const btnReset = document.getElementById("reset_time");
+    clearInterval(chrono);
 
-let chrono = 0; 
-let seconds = 0;
-let minutes = 0;
+})
+// reset button
+const btnReset = document.getElementById("reset_time").addEventListener("click", function () {
+    seconds = 0;
+    minutes = 0;
+    secondsPrint.innerText = "00";
+    minutesPrint.innerText = "00";
+})
+
+
 
 
