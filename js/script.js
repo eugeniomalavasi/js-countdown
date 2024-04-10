aaa6441fb8b6dfc6abb02eb76e7960dd579e5f05
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-04-10 14:57:06 +0200

    the countdown is now printed in the DOM

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
+// prelevo elemento da dom
+const hpnwyr = document.getElementById("countdown_print");
+
+
 let seconds = 11;
 const countDown = setInterval(() => {
     let result = "";
     if (seconds !== 1) {
         seconds--;
         console.log(seconds);
+        // output
+        hpnwyr.innerText = seconds;
     } else {
         result = "Buon Anno";
         clearInterval(countDown);
+        console.log(result);
+        // output
+        hpnwyr.innerText = result;
     }
-    console.log(result);
 }, 1000);
+
